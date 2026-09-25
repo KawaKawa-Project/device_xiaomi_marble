3a038f70abfb3d820bced328b27ea74d2ec99281
KawaKawa-Project <dcsnaff@gmail.com>
2026-09-25 08:07:31 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -84,3 +84,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+
+# Custom PowerHint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
